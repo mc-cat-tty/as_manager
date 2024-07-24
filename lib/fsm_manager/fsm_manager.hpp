0707acfd2fc9084a6d1b8cc14b54f9fc2d20a593
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-07-24 23:02:28 +0200

    Tested FSM Manager

--- lib/fsm_manager/fsm_manager.hpp
+++ lib/fsm_manager/fsm_manager.hpp
@@ -7,53 +7,56 @@
 
 #include "as/exception.hpp"
 
-namespace fsm {
+
+namespace as::fsm {
+
   /**
   * @brief Each node can either transition to the next node or transition
   * to himself (self-loop). In case of an exception, a pre-defined non-accepting
-  * state is reached.
+  * error state is reached.
   **/
   enum class NodeFlowCtrl {
     CURRENT,
     NEXT
   };
 
-  template <std::size_t NodesNumber> class Manager;
-
-  /// A node can be a function or a sub-manager (subset of independent nodes)
+  // A node can be a function or a sub-manager (subset of independent nodes)
   using Node = std::function<NodeFlowCtrl()>;
 
-  /**
-   * @brief fsm::Manager keeps a list of sequential nodes and a emergency node.
-   * The emergency node is supposed to handle all the actions for a fail-safe stop.
-   * The other states are executed sequentially; however, each state has the opportunity
-   * to decide whether to stay on current state or transition to the next one.
-  */
   template <std::size_t NodesNumber>
   class Manager {
     private:
     std::size_t currentNodeIdx;
     std::array<Node, NodesNumber> nodes;
-    Node emergencyNode;
+    Node errorNode;
+    std::atomic_bool errorHandler;
     
     public:
-    Manager(std::initializer_list<Node> nodeList, Node emergencyNode, unsigned entryIdx = 0) :
-      nodes(nodeList), emergencyNode(emergencyNode), currentNodeIdx(entryIdx) {}
+    Manager(std::array<Node, NodesNumber> nodeList, Node errorNode, unsigned entryIdx = 0) :
+      nodes(nodeList), errorNode(errorNode), currentNodeIdx(entryIdx) {}
     
     void run() {
-      auto currentNode = this->nodeList.at(currentNodeIdx);
+      auto currentNode = this->nodes.at(currentNodeIdx);
       auto retState = NodeFlowCtrl::CURRENT;
+
+      // If emergency occurs, FSM gets trapped in emergency state
+      if (this->errorHandler) {
+        this->errorNode();
+        return;
+      }
 
       try {
         retState = currentNode();
       }
       catch (as::EmergencyException e) {
+        this->errorHandler = true;
         std::cout << e.what() << std::endl;
-        this->emergencyNode();
+        this->errorNode();
       }
       catch (...) {
+        this->errorHandler = true;
         std::cout << "Major order problem occurred" << std::endl;
-        this->emergencyNode();
+        this->errorNode();
       }
       
       switch (retState) {
@@ -61,7 +64,7 @@ namespace fsm {
         break;
         
         case NodeFlowCtrl::NEXT:
-        ++this->currentNodeIdx;
+        if (this->currentNodeIdx < NodesNumber-1) ++this->currentNodeIdx;
         break;
 
         default:
@@ -71,6 +74,5 @@ namespace fsm {
     
     inline void operator() () { this->run(); }
   };
-
 
 };
