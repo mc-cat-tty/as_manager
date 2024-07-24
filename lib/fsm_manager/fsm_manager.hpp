f5b2942d1f71a24246eb96964245270fca5016dd
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-07-24 20:38:49 +0200

    Make it build

--- lib/fsm_manager/fsm_manager.hpp
+++ lib/fsm_manager/fsm_manager.hpp
@@ -3,39 +3,68 @@
 #include <utility>
 #include <variant>
 #include <initializer_list>
+#include <iostream>
+
+#include "as/exception.hpp"
 
 namespace fsm {
   /**
   * @brief Each node can either transition to the next node or transition
-  * to himself (self-loop). In case of an error, a pre-defined non-accepting
-  * state is reached.
+  * to himself (self-loop). In case of an exception, a pre-defined non-accepting
+  * error state is reached.
   **/
-  enum class NodeState {
-    CURRENT = 0,
-    NEXT,
-    ERROR
+  enum class NodeFlowCtrl {
+    CURRENT,
+    NEXT
   };
 
-  // A node can be a function or a sub-manaer (nodes subset)
-  using Node = std::variant<std::function<NodeState(int, float)>>;
+  template <std::size_t NodesNumber> class Manager;
 
-  template <std::initializer_list<Node> nodes>
-  struct Manager {
-    std::array<Node, std::size()> nodeList;
-    FSMNode error, finished;
-    unsigned idx;
+  // A node can be a function or a sub-manager (subset of independent nodes)
+  using Node = std::function<NodeFlowCtrl()>;
+
+  template <std::size_t NodesNumber>
+  class Manager {
+    private:
+    std::size_t currentNodeIdx;
+    std::array<Node, NodesNumber> nodes;
+    Node errorNode;
     
-    FSMManager(std::array<FSMNode, 10> nodeList, FSMNode error, FSMNode finished, unsigned entry = 0) :
-      nodeList(nodeList), error(error), finished(finished), idx(entry) {}
+    public:
+    Manager(std::initializer_list<Node> nodeList, Node errorNode, unsigned entryIdx = 0) :
+      nodes(nodeList), errorNode(errorNode), currentNodeIdx(entryIdx) {}
     
     void run() {
-      static FSMNode currentNode = this->nodeList.at(idx);
-      auto ret = currentNode();
-      if (ret == FSMState::CURRENT) ;
-      else if (ret == FSMState::NEXT) ++idx;
-      else if (ret == FSMState::ERROR) error();
-      else if (ret == FSMState::FINISHED) finished();
-      else std::unreachable();
+      auto currentNode = this->nodeList.at(currentNodeIdx);
+      auto retState = NodeFlowCtrl::CURRENT;
+
+      try {
+        retState = currentNode();
+      }
+      catch (as::EmergencyException e) {
+        std::cout << e.what() << std::endl;
+        this->errorNode();
+      }
+      catch (...) {
+        std::cout << "Major order problem occurred" << std::endl;
+        this->errorNode();
+      }
+      
+      switch (retState) {
+        case NodeFlowCtrl::CURRENT:
+        break;
+        
+        case NodeFlowCtrl::NEXT:
+        ++this->currentNodeIdx;
+        break;
+
+        default:
+        std::unreachable();
+      }
     }
+    
+    inline void operator() () { this->run(); }
   };
+
+
 };
