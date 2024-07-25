724ec1fe538847132e07080ad7b956444dbe370d
samuele-savazzi <145436605+samuele-savazzi@users.noreply.github.com>
2024-07-25 23:04:48 +0200

    fixed

--- lib/stream/ofstream.hpp
+++ lib/stream/ofstream.hpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <memory>
-#include <mutex>
 
 
 class OfStream {
 public:
+    // Metodo statico per ottenere l'istanza del singleton
     static OfStream& getInstance() {
         static OfStream instance;
         return instance;
@@ -22,10 +22,12 @@ public:
     }
 
 private:
+    // Costruttore privato per prevenire la creazione di istanze multiple
     OfStream() : outputStream(std::cout.rdbuf()) {}
 
     OfStream(const OfStream&) = delete;
     OfStream& operator=(const OfStream&) = delete;
 
+    // Stream di output
     std::ostream outputStream;
 };
