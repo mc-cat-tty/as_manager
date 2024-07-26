cb90d0601cc2039b0c5c611d99d7a507d484ddbb
samuele-savazzi <145436605+samuele-savazzi@users.noreply.github.com>
2024-07-26 18:45:22 +0200

    Adjust Signals class

--- lib/signals/signals.hpp
+++ lib/signals/signals.hpp
@@ -10,23 +10,27 @@ namespace signals {
     template <typename T>
     class Signal : public IUpdatable {
         public:
-            Signal(std::function<T()> source,  IUpdater* updater_, float alpha=1.0f) : low_pass_filter(alpha), hal(source), updater(updater_) {
+            Signal(std::function<T()> source,  IUpdater* updater_, float alpha=1.0f) : isFiltred(false),low_pass_filter(alpha), hal(source), updater(updater_) {
                 assert(alpha >= 0.0f && alpha <= 1.0f);
                 if(alpha < 1.0f) {
                     updater->registerSubscriber(this);
+                    isFiltred = true;  
                 }
             };
             T get_value() {
-                this->update();
-                return low_pass_filter.get_value();
+                if( isFiltred )
+                    return low_pass_filter.get_value();
+                else
+                    return hal();
             }
 
             void update() override {
-                low_pass_filter.update(hal());
+                low_pass_filter.update(static_cast<float>(hal()));
             }
 
         private:
             std::function<T()> hal;
+            bool isFiltred;
             LowPassFilter low_pass_filter;
             IUpdater* updater;
 
