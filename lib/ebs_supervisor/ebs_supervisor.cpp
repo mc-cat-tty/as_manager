343e48207cc2d4e1bae5fe22cf5a00154b4c2c8f
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-07-26 14:49:09 +0200

    Kickstart EBS supervisor configuration

--- lib/ebs_supervisor/ebs_supervisor.cpp
+++ lib/ebs_supervisor/ebs_supervisor.cpp
@@ -1,16 +1,28 @@
 #include <fsm_manager/fsm_manager.hpp>
-#include <fsm_manager/node_skeletons.hpp>
+#include <fsm_manager/nodes.hpp>
 #include <ebs_supervisor/ebs_supervisor.hpp>
+#include <temporal/common.hpp>
 #include <functional>
+
+std::string state;
 
 namespace as::ebs_supervisor {
     using namespace fsm;
+    using namespace std::chrono_literals;
 
     EbsSupervisor::EbsSupervisor() : ebsFsm (
         {
-          []() -> NodeFlowCtrl {},
-          []() -> NodeFlowCtrl {}
+          waitUntilNode([]{return false;}, "Mission selected and ASMS ON", "Waiting for mission and ASMS"),
+          doActionNode([]{state="CHECKING";}, "Published CHECKING"),
+          assertWithTimeoutNode([]{return false;}, 500ms, "Sufficient EBS tank pressure", "Waiting sufficient PEBS", "PEBS timeout"),
+          assertWithTimeoutNode([]{return false;}, 500ms, "Sufficient BRAKE pressure", "Waiting sufficient PBRAKE", "PBRAKE timeout"),
+          assertWithTimeoutNode([]{return false;}, 500ms, "SDC open pressure", "Waiting SDC opening", "SDC opening timeout"),
+          doActionNode([]{}, "Start toggling watchdog"),
+          doActionNode([]{}, "Close SDC"),
+          assertWithTimeoutNode([]{return false;}, 500ms, "SDC is close", "Waiting SDC closing", "SDC closing timeout"),
+          doActionNode([]{}, "Stop toggling watchdog"),
+          assertWithTimeoutNode([]{return false;}, 500ms, "SDC is open", "Waiting SDC opening", "SDC opening timeout")
         },
-        []() -> NodeFlowCtrl {}
+        terminalTrapNode([]{state="Emergency";}, "Emergency State")
     ) {}
 }
