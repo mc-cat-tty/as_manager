ff738920fe8a611145683028fbd3526a5e619b02
samuele-savazzi <145436605+samuele-savazzi@users.noreply.github.com>
2024-07-28 14:26:58 +0200

    Write Logstream template class

--- include/as_manager/stream/logstream.hpp
+++ include/as_manager/stream/logstream.hpp
@@ -2,32 +2,60 @@
 #include <fstream>
 
 namespace as {
-  class Logstream {
-    public:
-    // Metodo statico per ottenere l'istanza del singleton
-    static Logstream& getInstance() {
-        static Logstream instance;
-        return instance;
-    }
+ #include <iostream>
+#include <sstream>
+
+
+namespace as::logstream {
+  enum class OutputLog {
+    STANDARD = 0,
+    ROS
+  };
+
+template <OutputLog typeOfOutput = OutputLog::STANDARD>
+class Logstream : public std::ostream {
+public:
+    Logstream(std::string_view level) : std::ostream(&buf), level(level) {}
 
     template <typename T>
     Logstream& operator<<(const T& data) {
-        outputStream << data;
+        std::ostringstream oss;
+        oss << "[" << level << "] " << data;
+        buf.sputn(oss.str().c_str(), oss.str().size());
         return *this;
     }
 
-    void setOutputStream(std::ostream& stream) {
-        outputStream.rdbuf(stream.rdbuf());
+    Logstream& operator<<(std::ostream& (*manip)(std::ostream&)) {
+        if (manip == static_cast<std::ostream& (*)(std::ostream&)>(std::endl)) {
+            if constexpr (typeOfOutput == OutputLog::STANDARD) {
+                std::cout << std::endl;
+            } else {
+                std::cerr << std::endl;
+            }
+        }
+        return *this;
     }
 
-    private:
-    // Costruttore privato per prevenire la creazione di istanze multiple
-    Logstream() : outputStream(std::cout.rdbuf()) {}
+private:
+    class Buffer : public std::streambuf {
+    public:
+        int overflow(int c) override {
+            if (c != EOF) {
+                if constexpr (typeOfOutput == OutputLog::STANDARD) {
+                    std::cout.put(c);
+                } else {
+                    std::cerr.put(c);
+                }
+            }
+            return c;
+        }
+    };
 
-    Logstream(const Logstream&) = delete;
-    Logstream& operator=(const Logstream&) = delete;
+    Buffer buf;
+    std::string_view level;
+};
 
-    // Stream di output
-    std::ostream outputStream;
-  };
+  Logstream log=Logstream("LOG");
+  Logstream error=Logstream<OutputLog::ROS>("ERRORE");
+}
 }
