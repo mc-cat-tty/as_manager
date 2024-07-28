236d2fe0d191808ca2a39ff20f8a29efebd187e6
samuele-savazzi <145436605+samuele-savazzi@users.noreply.github.com>
2024-07-28 14:43:05 +0200

    Modified class logstrem in order to implement ros publisher

--- include/as_manager/stream/logstream.hpp
+++ include/as_manager/stream/logstream.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
-
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 namespace as::logstream {
   enum class OutputLog {
@@ -11,7 +12,7 @@ namespace as::logstream {
 template <OutputLog typeOfOutput = OutputLog::STANDARD>
 class Logstream : public std::ostream {
 public:
-    Logstream(std::string_view level) : std::ostream(&buf), level(level) {}
+    Logstream(std::string_view level, rclcpp::Publisher<std_msgs::msg::String>::SharedPtr ros_publisher = nullptr) : std::ostream(&buf), level(level), ros_publisher(ros_publisher) {}
 
     template <typename T>
     Logstream& operator<<(const T& data) {
@@ -25,8 +26,10 @@ public:
         if (manip == static_cast<std::ostream& (*)(std::ostream&)>(std::endl)) {
             if constexpr (typeOfOutput == OutputLog::STANDARD) {
                 std::cout << std::endl;
-            } else {
-                std::cerr << std::endl;
+            } else if (ros_publisher!=nullptr) {
+                std_msgs::msg::String msg;
+                msg.data = "\n";
+                ros_publisher->publish(msg);
             }
         }
         return *this;
@@ -40,7 +43,9 @@ private:
                 if constexpr (typeOfOutput == OutputLog::STANDARD) {
                     std::cout.put(c);
                 } else {
-                    std::cerr.put(c);
+                    std_msgs::msg::String msg;
+                    msg.data = std::string(c);
+                    ros_publisher->publish(msg);
                 }
             }
             return c;
@@ -49,9 +54,12 @@ private:
 
     Buffer buf;
     std::string_view level;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr ros_publisher;
 };
-
-  Logstream log=Logstream("LOG");
-  Logstream error=Logstream<OutputLog::ROS>("ERRORE");
-}
+  auto log_node= rclcpp::Node::make_shared("log_publisher_node");
+  auto log_publisher = log_node->create_publisher<std_msgs::msg::String>("log_topic", 10);
+  auto error_node= rclcpp::Node::make_shared("log_publisher_node");
+  auto error_publisher = error_node->create_publisher<std_msgs::msg::String>("error_topic", 10);
+  Logstream log=Logstream<OutputLog::ROS>("LOG",log_publisher);
+  Logstream error=Logstream<OutputLog::ROS>("ERRORE",error_publisher);
 }
