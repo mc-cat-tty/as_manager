8d57141f268ac7c207528cf17dfac906a42f41a6
LittleSnake99 <amir.tekaya@live.it>
2024-08-02 02:08:52 +0200

    Added config load in launch file
    Signed-off-by: LittleSnake99 <amir.tekaya@live.it>

--- launch/as_manager_launch.py
+++ launch/as_manager_launch.py
@@ -7,11 +7,14 @@ import os
 def generate_launch_description():
   launch_description = LaunchDescription()
   
+  config_node = os.path.join(get_package_share_directory('as_manager'), 'config', 'as_manager_conf.yaml')
+  
   node = Node(
     package="as_manager",
     name="as_manager_node",
     executable="as_manager_node",
-    output="screen"
+    output="screen",
+    parameters=[config_node]
   )
 
   launch_description.add_action(node)
