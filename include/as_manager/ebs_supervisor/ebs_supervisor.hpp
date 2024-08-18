31b0a4deefb935948517597fab84b628a2604ab7
Edoardo Torrini <edoardo.torrini@gmail.com>
2024-08-18 18:16:52 +0200

    porco dio the number of action

--- include/as_manager/ebs_supervisor/ebs_supervisor.hpp
+++ include/as_manager/ebs_supervisor/ebs_supervisor.hpp
@@ -20,6 +20,6 @@ namespace as::ebs_supervisor {
             EbsSupervisor(const EbsSupervisor&) = delete;
             EbsSupervisor(EbsSupervisor&&) = delete;
             EbsSupervisor& operator=(const EbsSupervisor&) = delete;
-            fsm::Manager<14> ebsFsm;
+            fsm::Manager<15> ebsFsm;
     };
 }
